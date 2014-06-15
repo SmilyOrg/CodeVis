cb732420805e9854f50d8a2851a41d25e9cefac1
Miha Lunar <mlunar@gmail.com>
2014-06-16 00:53:30 +0200

    added more test string escapes

--- bin/test/TestEscape.hx
+++ bin/test/TestEscape.hx
@@ -3,8 +3,8 @@ package ;
 class TestEscape {
 
 	public function new() {
-		var s:String = "abc \n \r \t \x21 \u003F \u{0021}123";
-		var s:String = 'abc \n \r \t \x21 \u003F \u{0021}123';
+		var s:String = "abc \" \\ \\\\ \\n \\a \\b \\c \n \r \t \x21 \u003F \u{0021}123";
+		var s:String = 'abc \' \\ \\\\ \\n \\a \\b \\c \n \r \t \x21 \u003F \u{0021}123';
 	}
 	
 }
